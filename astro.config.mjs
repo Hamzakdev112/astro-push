1ff495a97ecc47acd15d502894640928f88d0e32
Hamza <devhamzak@gmail.com>
2023-07-07 18:41:58 +0500

    Push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import worker from 'astro-service-worker/adapter';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: 'hybrid',
+  adapter: cloudflare()
 });
